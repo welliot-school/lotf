1e491c54695cda8ef4c19bd79fd2856702c782b3
welliot-school <elliotw28@columbusacademy.org>
2025-04-10 11:46:13 -0400

    Update lotf.js

--- lotf.js
+++ lotf.js
@@ -1,5 +1,16 @@
 const canvas = document.getElementById("lotf");
 const ctx = canvas.getContext("2d");
 
-ctx.font = "48px serif";
-ctx.fillText("Hello world!", 10, 50);
+//ctx.font = "48px serif";
+//ctx.fillText("Hello world!", 10, 50);
+
+const game = new Game();
+
+function update() {
+  game.update();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  game.draw(ctx);
+}
